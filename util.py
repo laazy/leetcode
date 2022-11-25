a07791f90068fadf7734292381091865e48387cc
laazy <laaazy@sjtu.edu.cn>
2022-11-25 17:00:31 +0800

    🔧 add list functions

--- util.py
+++ util.py
@@ -3,3 +3,23 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self) -> str:
+        node = self
+        res = []
+        while node is not None:
+            res.append(node.val)
+            node = node.next
+        return res.__str__()
+
+def build_list(l: list):
+    node = None
+    for i in reversed(l):
+        node = ListNode(i, node)
+    return node
