d06073165c605513bb7baf70efe851095ea33470
laazy <laaazy@sjtu.edu.cn>
2022-11-28 16:48:24 +0800

    ✔️ speed up by heap

--- 23.合并k个升序链表.py
+++ 23.合并k个升序链表.py
@@ -23,21 +23,25 @@ class Solution:
         lists = [i for i in lists if i]
         root = ListNode(0, None)
         node = root
+
+        def _tmp(this, other):
+            return this.val < other.val
+        ListNode.__lt__ = _tmp
+        heapq.heapify(lists)
+
         while lists:
-            min_idx, min_node = min(enumerate(lists), key=lambda x: x[1].val)
+            min_node = heapq.heappop(lists)
             if min_node.next:
-                lists[min_idx] = min_node.next
-            else:
-                lists.pop(min_idx)
+                heapq.heappush(lists, min_node.next)
             node.next = min_node
             node = node.next
         return root.next
 
 # @lc code=end
 
-# s = Solution().mergeKLists(lists = list(map(build_list, [[1,4,5],[1,3,4],[2,6]])))
-# print(s)
-# s = Solution().mergeKLists(lists = None)
-# print(s)
+s = Solution().mergeKLists(lists = list(map(build_list, [[1,4,5],[1,3,4],[2,6]])))
+print(s)
+s = Solution().mergeKLists(lists = None)
+print(s)
 s = Solution().mergeKLists(lists = [None, build_list([1])])
 print(s)
