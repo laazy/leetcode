65d279d412bfd12f2648fd9cb3fd007f7eb29622
laazy <laaazy@sjtu.edu.cn>
2022-11-28 16:06:37 +0800

    ✔️ speed up

--- 12.整数转罗马数字.py
+++ 12.整数转罗马数字.py
@@ -7,30 +7,34 @@
 # @lc code=start
 class Solution:
     def intToRoman(self, num: int) -> str:
-        d = {
-            1000: 'M',
-            900: 'CM',
-            500: 'D',
-            400: 'CD',
-            100: 'C',
-            90: 'XC',
-            50: 'L',
-            40: 'XL',
-            10: 'X',
-            9: 'IX',
-            5: 'V',
-            4: 'IV',
-            1: 'I'
-        }
-        s = ''
+        array = [
+            (1000, 'M'),
+            (900, 'CM'),
+            (500, 'D'),
+            (400, 'CD'),
+            (100, 'C'),
+            (90, 'XC'),
+            (50, 'L'),
+            (40, 'XL'),
+            (10, 'X'),
+            (9, 'IX'),
+            (5, 'V'),
+            (4, 'IV'),
+            (1, 'I')
+        ]
+        ans = ''
+        begin = 0
         while num:
-            for i in d:
-                if num >= i:
+            for idx, (n, s) in enumerate(array[begin:]):
+                if num >= n:
+                    begin = idx
                     break
-            num -= i
-            s += d[i]
-        return s
+            num -= n
+            ans += s
+        return ans
 # @lc code=end
+
+
 def s(*args, **kwargs):
     s = Solution().intToRoman(*args, **kwargs)
     print(s)
