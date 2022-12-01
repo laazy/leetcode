ef15f4447b08c64f97cc69e6d592a6f97785b7f0
laazy <laaazy@sjtu.edu.cn>
2022-12-01 15:49:46 +0800

    ✔️ add cache version

--- 10.正则表达式匹配.py
+++ 10.正则表达式匹配.py
@@ -7,6 +7,27 @@
 # @lc code=start
 class Solution:
     cache = {}
+
+
+    def isMatch2(self, s, p):
+        # use cache
+        if (s, p) in self.cache:
+            return self.cache[(s, p)]
+        if not p:
+            return not s
+        if p[-1] == '*':
+            if self.isMatch(s, p[:-2]):
+                self.cache[(s, p)] = True
+                return True
+            if s and (s[-1] == p[-2] or p[-2] == '.') and self.isMatch(s[:-1], p):
+                self.cache[(s, p)] = True
+                return True
+        if s and (p[-1] == s[-1] or p[-1] == '.') and self.isMatch(s[:-1], p[:-1]):
+            self.cache[(s, p)] = True
+            return True
+        self.cache[(s, p)] = False
+        return False
+
     def match_star(self, s: str, p: str):
         yield s
         for idx, i in enumerate(s):
