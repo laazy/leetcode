9c0459f4f0d49c3563b53c18c9f3024d2a157d16
laazy <laaazy@sjtu.edu.cn>
2022-12-05 15:35:53 +0800

    🎨 format

--- 10.正则表达式匹配.py
+++ 10.正则表达式匹配.py
@@ -16,8 +16,7 @@ class Solution:
             if j == len(pattern):
                 ans = i == len(text)
             else:
-                first_match = i < len(text) and pattern[j] in {
-                    text[i], '.'}
+                first_match = i < len(text) and pattern[j] in {text[i], '.'}
                 if j+1 < len(pattern) and pattern[j+1] == '*':
                     ans = dp(i, j+2) or first_match and dp(i+1, j)
                 else:
